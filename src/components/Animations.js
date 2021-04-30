5c6974cef3031a389d9c978a02cc0190e9cc66ed
github username <um_2003_8@hotmail.com>
2021-04-30 00:21:15 -0400

    create a div, make it round and add bouce effect

---
+++ src/components/Animations.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Animations = () => {
+    return(
+        <div className="container mx-auto py-20">
+            <div className="w-20 h-20 bg-black rounded-full animate-bounce"></div>
+        </div>
+    )
+}
+
+export default Animations
