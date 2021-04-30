8fbb215f7b0c4ef4664f7b4c6d67eb4f9b3a100c
github username <um_2003_8@hotmail.com>
2021-04-30 00:24:16 -0400

    add spin icon from fontawesome & give spin effect

--- src/components/Animations.js
+++ src/components/Animations.js
@@ -4,6 +4,11 @@ const Animations = () => {
     return(
         <div className="container mx-auto py-20">
             <div className="w-20 h-20 bg-black rounded-full animate-bounce"></div>
+
+            <div>
+                <i className="fas fa-spinner text-blue-900 text-2xl animate-spin px-3 py-3"></i>
+                Loading
+            </div>
         </div>
     )
 }
