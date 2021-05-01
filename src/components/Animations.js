7510ea837651629193f9af2209063ccd247cc060
github username <um_2003_8@hotmail.com>
2021-05-01 00:20:09 -0400

    add flex classes to style and make it responsive

--- src/components/Animations.js
+++ src/components/Animations.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Animations = () => {
     return(
-        <div className="container mx-auto py-20 px-20 lg:px-0 ">
+        <div className="container mx-auto py-20 px-20 lg:px-0 flex justify-evenly flex-col lg:flex-row">
             <h1 className="mb-10 text-4xl font-extrabold lg:self-center">Tailwind CSS Animations</h1>
             
             <div>
