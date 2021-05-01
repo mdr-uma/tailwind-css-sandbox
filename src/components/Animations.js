098d8526a733ad12fceedd3d6d0614f2f1f2fb6a
github username <um_2003_8@hotmail.com>
2021-05-01 00:21:19 -0400

    create a div with heading and ping effect

--- src/components/Animations.js
+++ src/components/Animations.js
@@ -9,6 +9,17 @@ const Animations = () => {
                 <h1 className="mb-10 text-2xl font-extrabold">Bounce</h1>
                 <div className="w-20 h-20 bg-black rounded-full animate-bounce"></div>
             </div>
+            
+            <div>
+                <h1 className="mb-10 text-2xl font-extrabold">Ping</h1>
+                <div>
+                    <span className="flex h-6 w-6">
+                        <span className="animate-ping absolute inline-flex h-6 w-6 rounded-full bg-purple-400 opacity-75"></span>
+                        <span className="relative inline-flex rounded-full h-6 w-6 bg-purple-500"></span>
+                    </span>
+                    Location
+                </div>
+            </div>
         </div>
     )
 }
