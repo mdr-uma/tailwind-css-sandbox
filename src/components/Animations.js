2abd3082d22eb72189b421898a850b34d5ee23b4
github username <um_2003_8@hotmail.com>
2021-05-01 00:17:46 -0400

    create a div that has heading and bounce effect

--- src/components/Animations.js
+++ src/components/Animations.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Animations = () => {
     return(
-        <div className="container mx-auto py-20 px-20 lg:px-0">
+        <div className="container mx-auto py-20 px-20 lg:px-0 ">
             <h1 className="mb-10 text-4xl font-extrabold lg:self-center">Tailwind CSS Animations</h1>
+            
+            <div>
+                <h1 className="mb-10 text-2xl font-extrabold">Bounce</h1>
+                <div className="w-20 h-20 bg-black rounded-full animate-bounce"></div>
+            </div>
         </div>
     )
 }
