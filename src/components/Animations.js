2e1b3717d3bc57b55516cbfd611b32232fca758a
github username <um_2003_8@hotmail.com>
2021-05-01 00:14:38 -0400

    add heading and style it

--- src/components/Animations.js
+++ src/components/Animations.js
@@ -2,13 +2,8 @@ import React from 'react'
 
 const Animations = () => {
     return(
-        <div className="container mx-auto py-20">
-            <div className="w-20 h-20 bg-black rounded-full animate-bounce"></div>
-
-            <div>
-                <i className="fas fa-spinner text-blue-900 text-2xl animate-spin px-3 py-3"></i>
-                Loading
-            </div>
+        <div className="container mx-auto py-20 px-20 lg:px-0">
+            <h1 className="mb-10 text-4xl font-extrabold lg:self-center">Tailwind CSS Animations</h1>
         </div>
     )
 }
