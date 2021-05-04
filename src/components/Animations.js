d3f89c02f4188a28a2334e4b7216438b36d82f58
github username <um_2003_8@hotmail.com>
2021-05-03 23:31:27 -0400

    change font-weight, remove unnecessary codes and add indents

--- src/components/Animations.js
+++ src/components/Animations.js
@@ -3,34 +3,36 @@ import React from 'react'
 const Animations = () => {
     return(
         <div className="container mx-auto py-20 px-20 lg:px-0 flex justify-evenly flex-col lg:flex-row">
-            <h1 className="mb-10 text-4xl font-extrabold lg:self-center">Tailwind CSS Animations</h1>
-            
+            <h1 className="mb-10 text-4xl font-extrabold lg:self-center">
+                Tailwind CSS Animations
+            </h1>
+
             <div>
-                <h1 className="mb-10 text-2xl font-extrabold">Bounce</h1>
+                <h1 className="mb-10 text-2xl font-bold">Bounce</h1>
                 <div className="w-20 h-20 bg-black rounded-full animate-bounce"></div>
             </div>
-            
+
             <div>
-                <h1 className="mb-10 text-2xl font-extrabold">Ping</h1>
+                <h1 className="mb-10 text-2xl font-bold">Ping</h1>
                 <div>
-                    <span className="flex h-6 w-6">
-                        <span className="animate-ping absolute inline-flex h-6 w-6 rounded-full bg-purple-400 opacity-75"></span>
-                        <span className="relative inline-flex rounded-full h-6 w-6 bg-purple-500"></span>
+                    <span>
+                        <span className="bg-purple-400 h-6 w-6 rounded-full absolute inline-flex opacity-75 animate-ping"></span>
+                        <span className="bg-purple-500 h-6 w-6 rounded-full relative inline-flex"></span>
+                        Active
                     </span>
-                    Location
                 </div>
             </div>
 
             <div>
-                <h1 className="mb-10 text-2xl font-extrabold">Spinner</h1>
+                <h1 className="mb-10 text-2xl font-bold">Spinner</h1>
                 <div>
-                    <i className="fas fa-spinner text-blue-900 text-2xl animate-spin px-3 py-3"></i>
+                    <i className="fas fa-spinner text-blue-900 text-2xl mr-2 animate-spin"></i>
                     Loading
                 </div>
             </div>
 
             <div>
-                <h1 className="mb-10 text-2xl font-extrabold">Pulse</h1>
+                <h1 className="mb-10 text-2xl font-bold">Pulse</h1>
                 <div>
                     <i className="fas fa-heartbeat text-red-700 text-6xl animate-pulse"></i>
                 </div>
