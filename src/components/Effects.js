e19d503765be2937ec4b6925764936a859a9fc55
github username <um_2003_8@hotmail.com>
2021-05-08 21:24:25 -0400

    create a div, give a background color and keep it to center

--- src/components/Effects.js
+++ src/components/Effects.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Effects = () => {
     return(
-        
+        <div className="w-screen h-screen flex justify-center items-center bg-indigo-100">
+        </div>
     )
 }
 
