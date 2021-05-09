4abc5242d98a8336225e0412e08f27e444320a5a
github username <um_2003_8@hotmail.com>
2021-05-08 21:26:03 -0400

    add a div, give it a width, height, background color, opacity and set shadow to none

--- src/components/Effects.js
+++ src/components/Effects.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Effects = () => {
     return(
         <div className="w-screen h-screen flex justify-center items-center bg-indigo-100">
+            <div className="w-80 h-60 bg-indigo-900 opacity-75 shadow-none"></div>
         </div>
     )
 }
