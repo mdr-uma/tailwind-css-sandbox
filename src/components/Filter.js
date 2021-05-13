bc521ad4f52812ff86c39d8647e555d4e1083a3f
github username <um_2003_8@hotmail.com>
2021-05-12 22:35:23 -0400

    add h1 and style it

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,8 +3,13 @@ import Image from '../images/yomari.jpeg'
 
 const Filter = () => {
     return(
-        <div className="h-screen flex justify-center items-center">
-            <img src={Image} alt='food-pic'/>
+        <div>
+            <h1 className="text-center mt-14 pb-8 font-bold text-2xl">
+                Newari Popular Dish Yomari Set
+            </h1>
+            <div className="flex justify-center items-center">
+                <img src={Image} alt='food-pic'/>
+            </div>
         </div>
     )
 }
