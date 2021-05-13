b61d6c0b57035a9f5e8dd8206620f0d1b1a74f5d
github username <um_2003_8@hotmail.com>
2021-05-12 22:22:33 -0400

    add image

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Image from '../images/yomari.jpeg'
 
 const Filter = () => {
     return(
         <div>
-            
+            <img src={Image} alt='food-pic'/>
         </div>
     )
 }
