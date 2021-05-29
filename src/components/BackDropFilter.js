0f13dd77d5b5ad7b16ca3240b21577db0b76b003
github username <um_2003_8@hotmail.com>
2021-05-28 20:40:53 -0400

    add image to add backdrop filter

--- src/components/BackDropFilter.js
+++ src/components/BackDropFilter.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Image from '../images/wyoming-scenery.jpeg'
 
 const BackDropFilter = () => {
     return(
-        <div>
-            
+        <div className="mx-16 relative">
+            <div className="absolute w-full">
+                <img src={Image} alt='' className="w-full"/>
+            </div>
         </div>
     )
 }
