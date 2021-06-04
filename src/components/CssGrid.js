23bc1fcce40dd388f79641555dace0cb48786279
github username <um_2003_8@hotmail.com>
2021-06-03 23:04:01 -0400

    add background color to divs

--- src/components/CssGrid.js
+++ src/components/CssGrid.js
@@ -3,17 +3,17 @@ import React from 'react'
 const CssGrid = () => {
     return(
         <div className='grid grid-cols-4 gap-1'>
-            <div className='text-center text-white py-6 col-span-3'>1</div>
-            <div className='text-center text-white py-2 row-span-6'>2</div>
-            <div className='rounded text-center text-white py-6 row-span-2'>11</div>
-            <div className='rounded text-center text-white py-16'>12</div>
-            <div className='rounded text-center text-white py-6 row-span-2'>14</div>
-            <div className='rounded text-center text-white py-6'>15</div>
-            <div className='rounded text-center text-white py-6'>16</div>
-            <div className='rounded text-center text-white py-6 row-span-2'>17</div>
-            <div className='rounded text-center text-white py-6 row-span-2'>19</div>
-            <div className='rounded text-center text-white py-6'>20</div>
-            <div className='text-center text-white py-16 col-span-3'>24</div>
+            <div className='bg-blue-500 text-center text-white py-6 col-span-3'>1</div>
+            <div className='bg-blue-500  text-center text-white py-2 row-span-6'>2</div>
+            <div className='bg-blue-500 rounded text-center text-white py-6 row-span-2'>11</div>
+            <div className='bg-blue-500 rounded text-center text-white py-16'>12</div>
+            <div className='bg-blue-500 rounded text-center text-white py-6 row-span-2'>14</div>
+            <div className='bg-blue-500 rounded text-center text-white py-6'>15</div>
+            <div className='bg-blue-500 rounded text-center text-white py-6'>16</div>
+            <div className='bg-blue-500 rounded text-center text-white py-6 row-span-2'>17</div>
+            <div className='bg-blue-500 rounded text-center text-white py-6 row-span-2'>19</div>
+            <div className='bg-blue-500 rounded text-center text-white py-6'>20</div>
+            <div className='bg-blue-500 text-center text-white py-16 col-span-3'>24</div>
         </div>
 
     )
